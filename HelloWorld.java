2e8dcd92b94d4a6b3ba2cd99a5b86d57d0d9e322
jacobwu-b <jacobwu@berkeley.edu>
2020-04-16 22:56:58 +0000

    Finish lesson

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
+
+/* Use `javac HelloWorld.java` to compile
+ * Use `java HelloWorld` to run
+ */
