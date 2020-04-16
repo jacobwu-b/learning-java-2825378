ab6132eca3d6b3e8d2da1693fd22720f55cf9635
jacobwu-b <jacobwu@berkeley.edu>
2020-04-16 23:17:39 +0000

    Finish lesson

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    int studentAge = 15;
+    double studentGPA = 3.45;
+    char studentFirstInitial = 'K';
+    char studentLastInitial = 'h';
+    boolean hasPerfectAttendance = true;
+
+    String studentName = "Jacob Wu";
+
+    System.out.println(studentAge);
+    System.out.println(studentName);
+  }
+}
