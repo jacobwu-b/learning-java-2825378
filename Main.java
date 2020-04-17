8eb05e516ee6b2a7b9d77d28191605287f059ddf
jacobwu-b <jacobwu@berkeley.edu>
2020-04-17 19:02:41 +0000

    finish lesson

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    Triangle triangleA = new Triangle(15,15,15,5,5);
+    Triangle triangleB = new Triangle(3,3.2,3,3.7,3.8);
+
+    System.out.println(triangleA.findArea());
+  }
+}
